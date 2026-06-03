0a1e93243fa7eeffa7185f89e8ae7e01f702288f
EricsonCR <ericson4634@gmail.com>
2026-06-02 22:45:49 -0500

    Creacion Proyecto

---
+++ src/main/java/com/ericson/tiendasmartshop/TiendaSmartShopApplication.java
@@ -0,0 +1,13 @@
+package com.ericson.tiendasmartshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaSmartShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaSmartShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ericson/tiendasmartshop/TiendaSmartShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericson.tiendasmartshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaSmartShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
